35b14c75d86197ffba011fedc1cb02fde15808ab
Peter Williams <petercw94@gmail.com>
2020-04-15 18:23:44 -0600

    New script for tidying the survey data.

---
+++ Who_Is_Smarter/survey_tidying_script.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+# Load the data
+df = read.csv("./Are You Smarter Survey RAW SP2019.csv")
+# Remove the unnecessary columns
+nocol = c(1,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,31,32,33,34,35,36,38,39,40,42,43)
+df.new = df[,-nocol]
+
+# Rename the columns
+newNames = c("Date", "Name", "Name1", "Name2", "Percentage_smarter_than", "Group_work?", "Group_member_name", "Group_interactions", "Who_is_smarter?", "How_do_you_know_who_is_smarter?", "Gender", "uvid", "courseID", "courseName", "instructor_last_name", "major_description", "gender2")
+colnames(df.new) = newNames
+
+# Remove the unnecessary rows
+df.new = df.new[-c(1:3), ]
+
+# Save the file as a .csv
+write.csv("./tidy_data/Smarter_survey_clean_SP2019.csv")
--- Who_Is_Smarter/tidying_script.R
+++
@@ -1,7 +0,0 @@
-library(tidyverse)
-
-
-df = read.csv("./Are You Smarter Survey RAW SP2019.csv")
-
-newNames = c("Date", "Name", "Name1", "Name2", "Group_work?", "Group_member_name", "Group_interactions", "Who_is_smarter?", "How_do_you_know_who_is_smarter?", "Gender", "uvid", "courseID", "courseName", "instructor_last_name", "major_description", "gender2")
-df[-2, ]
